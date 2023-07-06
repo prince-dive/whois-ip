3bffd5266a637a4ee6cdcf1d6a8656cc0206631f
Prince <72026841+prince-dive@users.noreply.github.com>
2023-07-06 20:10:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,42 @@ import asyncio
 
 
 
-async def whois_info(domain):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f'http://ip-api.com/json/{domain}') as response:
+class whois_ip:
+    async def info_choice(domain, choi):
+        if choi == "json":
+            send_task = asyncio.create_task(whois_ip.whois_json(domain))
+            await send_task
+        elif choi == "info":
+            send_task = asyncio.create_task(whois_ip.whois_inf(domain))
+            await send_task
+        else:
+            print("NONE")
 
-            html = await response.json()
-            if html['status'] =='success':
-                result = (f"Country:{html['country']}\nRegion:{html['regionName']}\nzip:{html['zip']}\nisp:{html['isp']}\nip:{html['query']}\n")
-                print(result)
-            else:
-                print("None domain")
+
+
+    async def whois_json(domain):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'http://ip-api.com/json/{domain}') as response:
+
+                html = await response.json()
+                if html['status'] =='success':
+                    print(html)
+                else:
+                    print("None domain")
+
+
+    async def whois_inf(domain):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'http://ip-api.com/json/{domain}') as response:
+
+                html = await response.json()
+                if html['status'] =='success':
+                    result = (f"Country:{html['country']}\nRegion:{html['regionName']}\nzip:{html['zip']}\nisp:{html['isp']}\nip:{html['query']}\n")
+                    print(result)
+                else:
+                    print("None domain")
+
+
 
 
 
@@ -20,5 +46,7 @@ async def whois_info(domain):
 
 
 if __name__ == "__main__":
+    domain = input("Enter the ip or domain to display the whois: ")
+    choice = input("Enter the info or json: ")
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(whois_info(input("Enter the ip or domain to display the whois:")))
+    loop.run_until_complete(whois_ip.info_choice(domain=domain, choi=choice))
